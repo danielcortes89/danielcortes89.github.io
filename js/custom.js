3a6aa52e4c6ce5029866a8abcc7900fe00e24fe8
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-02-28 20:23:59 -0600

    feat: Responsive header appears

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(
     console.log('TEST')
 )
+
+const triggerResponsive = () => {
+    let ghostNav = document.getElementById("ghost-nav")
+    if(ghostNav.style.display === "block"){
+        ghostNav.style.display = "none"
+    } else {
+        ghostNav.style.display = "block"
+    }
+}
